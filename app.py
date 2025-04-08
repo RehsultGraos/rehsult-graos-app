dafd0a994c561fb7665c3d100774ead73137241b
RehsultGraos <mesqferreira@gmail.com>
2025-04-08 10:52:46 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from fpdf import FPDF
+from fpdf2 import FPDF
 from io import BytesIO
 from math import pi
 
